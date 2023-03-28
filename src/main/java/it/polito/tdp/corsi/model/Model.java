5c6d2411f38564a2ef642ac09bc2e7ac8a49a1d8
matteopalazzo <matteopalazzo@mbp-di-matteo.station>
2023-03-28 17:09:57 +0200

    implementato il primo metodo del controller

--- src/main/java/it/polito/tdp/corsi/model/Model.java
+++ src/main/java/it/polito/tdp/corsi/model/Model.java
@@ -1,7 +1,26 @@
 package it.polito.tdp.corsi.model;
 
+import java.util.List;
+
+import it.polito.tdp.corsi.db.CorsoDao;
 
 public class Model {
+	
+	private CorsoDao corsoDao;
+	
+	
+	public Model() {
+		super();
+		this.corsoDao = new CorsoDao();
+	}
+
+
+	public List<Corso> getCorsiByPeriodo(int periodo){
+		return this.corsoDao.getCorsiByPeriodo(periodo);
+		
+		
+		
+	}
 	
 	
 }
